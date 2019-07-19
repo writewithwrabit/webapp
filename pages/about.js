deb562d5f67290e7d221c37721da0c425ab664e4
Anthony Morris <anthony@amorrissound.com>
2019-07-18 19:54:06 -0700

    Initialize Next project

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import withLayout from '../components/Layout';
+
+const About = () => (
+    <p>This is the about page.</p>
+);
+
+export default withLayout(About);
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import withLayout from '../components/Layout';
+
+const Index = () => (
+    <p>Hello Next.js</p>
+);
+
+export default withLayout(Index);
