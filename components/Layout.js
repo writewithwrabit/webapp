f2409de3307092124d581fdb05296b1bf941e34a
Anthony Morris <anthony@amorrissound.com>
2019-07-22 16:44:04 -0700

    Style the login page

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,15 @@
 import Header from './Header';
+import Router from 'next/router';
 
 const withLayout = Page => {
+
   return () => (
     <div className="container mx-auto min-h-screen">
-      <Header />
+      {
+        Router.pathname !== '/login'
+          ? <Header />
+          : ''
+      }
       <Page />
     </div>
   );
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import firebase from '../firebase';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useStoreState, useStoreActions } from 'easy-peasy';
 
@@ -36,12 +37,48 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input placeholder="Email" value={email} type="email" onChange={({ target }) => setEmail(target.value)} />
-        <input placeholder="Password" value={password} type="password" onChange={({ target }) => setPassword(target.value)} />
-        <input type="submit" value="Submit" />
-      </form>
+    <div className="flex flex-col justify-center items-center p-20">
+      <div className="pb-10 text-5xl font-extrabold">
+        🐇 wrabit
+      </div>
+
+      <div className="w-full max-w-md bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="pb-5 mb-5 border-b-2 border-gray-100 text-center">
+          Welcome back!
+        </div>
+
+        <form>
+          <div className="mb-4">
+            <label className="hidden" for="email">
+              Email
+            </label>
+            <input className="shadow-inner border rounded w-full py-2 px-3 text-gray-700  focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Email" />
+          </div>
+
+          <div className="mb-6">
+            <label className="hidden" for="password">
+              Password
+            </label>
+            <input className="shadow-inner border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+          </div>
+
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Sign In
+            </button>
+            <a className="inline-block align-baseline text-sm text-blue-500 hover:text-blue-800" href="#">
+              Forgot your password?
+            </a>
+          </div>
+        </form>
+      </div>
+
+      <p className="text-center text-sm">
+        Don't have an account?&nbsp;
+        <Link href="/signup">
+          <a>Sign Up</a>
+        </Link>
+      </p>
     </div>
   );
 };
