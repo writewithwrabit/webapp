d6d22f953703b65603d2445344df7029dba31c5c
Anthony Morris <anthony@amorrissound.com>
2019-07-23 16:52:15 -0700

    Add Sentry

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
 import App, { Container } from 'next/app';
 import dynamic from 'next/dynamic';
+
+Sentry.init({dsn: "https://b0529282a1ce4acd9e9f47d2e631ccd4@sentry.io/1511977"});
 
 import { StoreProvider } from 'easy-peasy';
 import store from '../store/store';
