7626ac1df3e9426e1b93492481c9e627a05f96dc
Anthony Morris <anthony@amorrissound.com>
2019-09-29 16:02:16 -0700

    Stripe Setup (#15)
    * Start refactoring sign up page
    
    * Add plans page
    
    * Minor formatting and price adjustment
    
    * Create the user so Stripe ID exists
    
    * Add credit card form for plans
    
    * Complete first pass of signup flow

---
+++ components/Plans.js
@@ -0,0 +1,88 @@
+import Payment from './Payment';
+
+const Plans = ({ setPlan, plan, user, completeSignup }) => {
+
+
+  return (
+    <div className="flex flex-col items-center pt-16">
+      <div className="text-center pb-6 text-5xl font-extrabold">
+        wrabit
+      </div>
+
+      <div className="text-center mb-10 w-1/4">
+        Support mental health research while building your own writing habit for <span className="font-bold">less than a quarter a day</span>!
+      </div>
+
+      <div className="flex justify-center p-4 text-center w-4/5">
+        <div className="w-full max-w-md mr-6 bg-white shadow-md rounded px-8 pt-6 pb-8 flex flex-col justify-between items-center">
+          <div className="text-3xl">
+            Monthly Subscription
+          </div>
+
+          <div className="my-6">
+            <div className="font-extrabold text-6xl">$6.99</div>
+            <div className="text-gray-600">per month</div>
+          </div>
+
+          <ul>
+            <li>30 day free trial</li>
+            <li>Billed monthly</li>
+            <li>We donate $1 for every 14 day streak</li>
+          </ul>
+
+          {
+            plan === 'monthly'
+              ? <Payment user={user} plan="plan_FsvZtUpz9xw0Tx" completeSignup={completeSignup} />
+              : (
+                <button
+                  className="text-blue-500 hover:text-blue-700 font-bold p-4 mt-6 rounded border-blue-500 border-2 hover:border-blue-700 focus:outline-none focus:shadow-outline w-full"
+                  type="button"
+                  onClick={() => setPlan('monthly')}
+                >
+                  Subscribe Monthly
+                </button>
+              )
+          }
+        </div>
+
+        <div className="w-full max-w-md ml-6 bg-white shadow-md rounded px-8 pt-6 pb-8 flex flex-col justify-between items-center relative">
+          <div className="mx-auto rounded-full bg-blue-500 text-white absolute top-0 -mt-4 text-sm py-2 px-4">
+            28% cheaper
+          </div>
+
+          <div className="text-3xl">
+            Yearly Subscription
+          </div>
+
+          <div className="my-6">
+            <div className="font-extrabold text-6xl">$4.99</div>
+            <div className="text-gray-600">per month</div>
+          </div>
+
+          <ul>
+            <li>50% more social impact</li>
+            <li>30 day free trial</li>
+            <li>Billed yearly</li>
+            <li>We donate $1 for every 7 day streak</li>
+          </ul>
+
+          {
+            plan === 'yearly'
+              ? <Payment user={user} plan="plan_FtuSSAndZnWxET" completeSignup={completeSignup} />
+              : (
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 mt-6 rounded border-blue-500 border-2 hover:border-blue-700 focus:outline-none focus:shadow-outline w-full"
+                  type="button"
+                  onClick={() => setPlan('yearly')}
+                >
+                  Subscribe Yearly
+                </button>
+              )
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Plans;
--- pages/signup.js
+++ pages/signup.js
@@ -1,198 +1,77 @@
 import { useState } from 'react';
-import Link from 'next/link';
+import Head from 'next/head';
 import { ApolloConsumer } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
+import { useMutation } from '@apollo/react-hooks';
+import { useRouter } from 'next/router';
+import gql from 'graphql-tag';
+
+const UPDATE_USER = gql`
+  mutation UpdateUser($id: ID!, $firebaseId: String) {
+    updateUser(input: { id: $id, firebaseID: $firebaseId }) {
+      id
+      firebaseID
+    }
+  }
+`;
 
 import firebase from '../firebase';
 
+import SignupUser from '../components/SignupUser';
+import Plans from '../components/Plans';
+
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [updateUser] = useMutation(UPDATE_USER);
+  const [stage, setStage] = useState('signup');
+  const [user, setUser] = useState({});
+  const [plan, setPlan] = useState({});
 
-  const handleSubmit = (e, client) => {
-    e.preventDefault();
+  const stageComponent = {
+    signup: SignupUser,
+    plans: Plans,
+  }
 
-    if (password !== passwordConfirmation) {
-      console.log('Password does not match password confirmation!');
-      return;
-    }
-
+  const completeSignup = () => {
     firebase
       .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(({ user }) => {
-        const { uid: firebaseID } = user;
+      .createUserWithEmailAndPassword(user.email, user.password)
+      .then(async ({ user: firebaseUser }) => {
+        const router = useRouter();
+        const { uid: firebaseId } = firebaseUser;
 
-        client.mutate({
-          mutation: gql`
-            mutation CreateUser($firebaseID: String!, $firstName: String!, $lastName: String, $email: String!) {
-              createUser(input: { firebaseID: $firebaseID, firstName: $firstName, lastName: $lastName, email: $email }) {
-                id
-              }
-            }
-          `,
-          variables: { firebaseID, firstName, lastName, email },
-          update: (cache, { data: { createUser } }) => {
-            cache.writeQuery({
-              query: gql`
-                query GetUser {
-                  user
-                }
-              `,
-              data: {
-                user: createUser,
-              },
-            });
-          }
-        });
+        await updateUser({ variables: { id: user.id, firebaseId }});
+
+        router.push('/write');
       })
       .catch((error) => {
         console.log(error.code, error.message);
     });
   };
 
+  const Component = stageComponent[stage];
+
   return (
-    <ApolloConsumer>
-      {
-        client => (
-          <div className="flex justify-center p-4 pt-16">
-      <div className="mr-20 max-w-md px-20 hidden md:block">
-        <div className="text-4xl font-extrabold pb-8">
-          wrabit
-        </div>
-
-        <div className="text-sm">
-          <div className="pb-5">
-            <p className="font-bold pb-2 -ml-6">✍️ Build your habit</p>
-            <p>Build your writing habit by one day at a time. Set a daily goal and work your way up to it.</p>
-          </div>
-
-          <div className="pb-5">
-            <p className="font-bold pb-2 -ml-6">🗓️ Free for 30 days</p>
-            <p>See if you like us, risk free. After 30 days, subscribe monthly for the price of a coffee.</p>
-          </div>
-
-          <div className="pb-5">
-            <p className="font-bold pb-2 -ml-6">💬 Community Support</p>
-            <p>Engage with the community to help you stay motivated and make writing fun.</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full max-w-md">
-        <div className="text-4xl text-center font-extrabold pb-8 md:hidden">
-          wrabit
-        </div>
-
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="pb-5 mb-5 border-b-2 border-gray-100 text-center">
-            Create your Wrabit account now
-          </div>
-
-          <form onSubmit={event => handleSubmit(event, client)}>
-            <div className="mb-4">
-              <label className="hidden" htmlFor="email">
-                Email
-              </label>
-
-              <input
-                className="shadow-inner border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline"
-                id="email"
-                placeholder="Email" 
-                type="email"
-                value={email}
-                onChange={({ target }) => setEmail(target.value)} 
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="hidden" htmlFor="first-name">
-                First Name
-              </label>
-
-              <input
-                className="shadow-inner border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
-                id="first-name"
-                type="text"
-                placeholder="First Name"
-                value={firstName}
-                onChange={({ target }) => setFirstName(target.value)} 
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="hidden" htmlFor="last-name">
-                Last Name
-              </label>
-
-              <input
-                className="shadow-inner border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline"
-                id="last-name"
-                type="text"
-                placeholder="Last Name"
-                value={lastName}
-                onChange={({ target }) => setLastName(target.value)} 
-              />
-            </div>
-
-            <div className="mb-4">
-              <label className="hidden" htmlFor="password">
-                Password
-              </label>
-
-              <input
-                className="shadow-inner border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={({ target }) => setPassword(target.value)}
-              />
-            </div>
-
-            <div className="mb-6">
-              <label className="hidden" htmlFor="confirm-password">
-                Confirm Password
-              </label>
-
-              <input
-                className="shadow-inner border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline"
-                id="confirm-password"
-                type="password"
-                placeholder="Confirm Password"
-                value={passwordConfirmation}
-                onChange={({ target }) => setPasswordConfirmation(target.value)}
-              />
-            </div>
-
-            <div className="flex items-center justify-center">
-              <button
-                className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                type="button"
-                onClick={event => handleSubmit(event, client)}
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-        </div>
-
-        <div>
-          <p className="text-center text-sm">
-            Already have an account?&nbsp;
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          </p>
-        </div>
-      </div>
+    <div>
+      <Head>
+        {/* You must import the Stripe.js library from Stripe for compliance reasons */}
+        <script src="https://js.stripe.com/v3/"></script>
+      </Head>
+      
+      <ApolloConsumer>
+        {
+          client => (
+            <Component
+              client={client}
+              setUser={setUser}
+              user={user}
+              setStage={setStage}
+              setPlan={setPlan}
+              plan={plan}
+              completeSignup={completeSignup}
+            />
+          )
+        }
+      </ApolloConsumer>
     </div>
-        )
-      }
-    </ApolloConsumer>
   );
 };
 
