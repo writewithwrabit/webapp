390e31ad4356826775154c168015d8bdeb35917e
Anthony Morris <anthony@amorrissound.com>
2019-10-01 18:50:06 -0700

    Add Sqreen badge to the homepage (#18)
    * Add Sqreen badge to the homepage
    
    * Fix style tag

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import withLayout from '../components/Layout';
 const Index = () => (
   <div>
     Welcome to Wrabit!
+
+    <a title="Realtime application protection" href="https://www.sqreen.com/?utm_source=badge">
+      <img style={{ width: 109, height: 36 }} src="https://s3-eu-west-1.amazonaws.com/sqreen-assets/badges/20171107/sqreen-mono-badge.svg" alt="Sqreen | Runtime Application Protection" />
+    </a>
   </div>
 );
 
