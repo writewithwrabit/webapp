6c9e5b052a8319c868adde2f14f5054605621263
Anthony Morris <anthony@amorrissound.com>
2019-10-08 19:28:39 -0700

    Adjust OG tags

--- seo.config.js
+++ seo.config.js
@@ -1,10 +1,11 @@
 export default {
   openGraph: {
-    title: 'Wrabit',
+    title: 'Write With Wrabit',
     type: 'website',
+    description: 'Wrabit helps you build a daily writing habit, one small step at a time. Join a community of others as you develop a healthier relationship with yourself.',
     url: 'https://writewithwrabit.com',
     site_name: 'Wrabit',
-    images: [          {
+    images: [{
       url: 'https://writewithwrabit.com/static/og.png',
       alt: 'Write With Wrabit',
     }]
