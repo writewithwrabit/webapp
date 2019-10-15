db860ea580a625ab97971643c7dcbf5bec7cf212
Anthony Morris <anthony@amorrissound.com>
2019-10-15 13:13:00 -0700

    Use new GraphQL query to get daily entry

--- pages/write.js
+++ pages/write.js
@@ -1,32 +1,37 @@
 import gql from "graphql-tag";
 import { useQuery } from '@apollo/react-hooks';
 import { useStoreState } from 'easy-peasy';
+import { startOfDay } from 'date-fns';
 
 import withLayout from '../components/Layout';
 
 import Editor from '../components/Editor';
 
 const GET_ENTRY = gql`
-  query LatestEntry($userID: ID!) {
-    latestEntry(userID: $userID) {
+  query DailyEntry($userID: ID!, $date: String!) {
+    dailyEntry(userID: $userID, date: $date) {
       id
       content
       wordCount
+      createdAt
     }
   }
 `;
 
 const Write = () => {
   const { uid: userID } = useStoreState(state => state.user).firebaseData;
+
+  const date = startOfDay(new Date());
+
   const { loading, error, data } = useQuery(GET_ENTRY, {
-    variables: { userID },
+    variables: { userID, date },
   });
 
   if (loading) return (<div>LOADING</div>);
   if (error) return (<div>ERROR</div>);
-
+  console.log(data);
   return (
-    <Editor entry={data.latestEntry} />
+    <Editor entry={data.dailyEntry} />
   );
 }
 
