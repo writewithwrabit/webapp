18c99e5fdd41c353503a389e3aa6c44c82b3d7e8
Anthony Morris <anthony@amorrissound.com>
2019-10-17 19:58:18 -0700

    Adjust the header style

--- components/Header.js
+++ components/Header.js
@@ -12,43 +12,45 @@ const Header = () => {
   const user = useStoreState(state => state.user);
 
   return (
-    <div className="nav my-4 border-b-2 border-gray-200 flex text-gray-600">
-      <div className="pb-4 w-1/3">
-        <Link href="/">
-          <a className="font-extrabold">
-            🐇wrabit
-          </a>
-        </Link>
-      </div>
+    <div className="border-b-2 border-primary pt-4 mb-4 bg-gray-800">
+      <div className="container mx-auto nav flex text-white">
+        <div className="pb-4 w-1/3">
+          <Link href="/">
+            <a className="font-extrabold">
+              🐇wrabit
+            </a>
+          </Link>
+        </div>
 
-      <div className="w-1/3 text-center">
-        {
-          user.isAuthenticated
-            ? (
-              <span>
-                <NavItem url="/entries" text="Entries" />
-
-                <NavItem url="/write" text="Write" />
-
-                <NavItem url="/stats" text="Stats" />
-              </span>
-            )
-            : ''
-        }
-      </div>
-
-      <div className="w-1/3  text-right">
-        <span className="nav-item pb-4">
+        <div className="w-1/3 text-center">
           {
             user.isAuthenticated
-              ? <SettingsMenu />
-              : (
-                <Link href="/login">
-                  <a className="px-8">Login</a>
-                </Link>
+              ? (
+                <span>
+                  <NavItem url="/entries" text="Entries" />
+
+                  <NavItem url="/write" text="Write" />
+
+                  <NavItem url="/stats" text="Stats" />
+                </span>
               )
+              : ''
           }
-        </span>
+        </div>
+
+        <div className="w-1/3  text-right">
+          <span className="nav-item pb-4">
+            {
+              user.isAuthenticated
+                ? <SettingsMenu />
+                : (
+                  <Link href="/login">
+                    <a className="px-8">Login</a>
+                  </Link>
+                )
+            }
+          </span>
+        </div>
       </div>
     </div>
   );
