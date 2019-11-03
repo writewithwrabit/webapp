f48a2b29892a1d1c233c5f640520b84e44a18d3c
Anthony Morris <anthony@amorrissound.com>
2019-11-02 20:41:26 -0700

    Enhance stat selector

--- components/StatsSelector.js
+++ components/StatsSelector.js
@@ -1,19 +1,24 @@
 import {useState} from 'react';
+import styled from '@emotion/styled';
 
-const StatsPill = ({ children, selected, name, onClick }) => (
-  <span
+const StyledSpan = styled.span`
+  cursor: pointer;
+`;
+
+const StatsPill = ({children, selected, name, onClick}) => (
+  <StyledSpan
     onClick={() => onClick(name)}
-    className={'rounded-full p-2 w-1/2 text-center ' + (selected === name ? 'bg-primary font-bold' : '')}
+    className={'rounded-full p-2 w-1/2 text-center ' + (selected === name ? 'bg-primary font-bold text-white shadow-md' : '')}
   >
     {children}
-  </span>
+  </StyledSpan>
 );
 
 const StatsSelector = () => {
   const [selected, setSelected] = useState('me');
 
   return (
-    <div className="bg-gray-300 w-1/5 rounded-full flex justify-between text-sm mx-auto mb-6">
+    <div className="bg-gray-300 w-1/5 rounded-full flex justify-between text-sm mx-auto mb-6 shadow-inner">
       <StatsPill selected={selected} name="me" onClick={setSelected}>
         Me
       </StatsPill>
