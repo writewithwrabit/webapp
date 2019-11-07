8371f23c899d97c9a37f4708555aa38cc2c6ef76
Anthony Morris <anthony@amorrissound.com>
2019-11-06 22:15:05 -0800

    Dynamically load large components

--- pages/write.js
+++ pages/write.js
@@ -1,11 +1,15 @@
 import { Suspense } from 'react';
 import { startOfDay } from 'date-fns';
+import dynamic from 'next/dynamic';
 
 import GetEntry from '../queries/GetEntry';
 
 import withLayout from '../components/Layout';
 import withPreloadedQuery from '../components/PreloadedQuery';
-import Editor from '../components/Editor';
+const Editor = dynamic(
+  () => import('../components/Editor'),
+  { ssr: false }
+);
 
 const Write = () => (
   <Suspense fallback={<div>Loading...</div>}>
