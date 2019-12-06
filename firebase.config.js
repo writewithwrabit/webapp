dd7625137f241262a8f9da96d34907080b67cc2c
Anthony Morris <anthony@amorrissound.com>
2019-12-06 14:24:15 -0800

    Add firebase config file (#95)
    * Add firebase config file
    
    * Use an env variable for switching between firebase config

---
+++ firebase.config.js
@@ -0,0 +1,25 @@
+const stage = {
+  apiKey: "AIzaSyCyq9K36z_LF0yUhp9XaHJTDUdwBszVP0I",
+  authDomain: "wrabit-stage.firebaseapp.com",
+  databaseURL: "https://wrabit-stage.firebaseio.com",
+  projectId: "wrabit-stage",
+  storageBucket: "wrabit-stage.appspot.com",
+  messagingSenderId: "122086424917",
+  appId: "1:122086424917:web:15682fccd783eef956efdc",
+  measurementId: "G-ZRZDHVWFYV"
+};
+
+const prod = {
+  apiKey: "AIzaSyD8oE6wim8fdgXW1kg4zFPdwVsqtUdN7Y4",
+  authDomain: "wrabit-webapp.firebaseapp.com",
+  databaseURL: "https://wrabit-webapp.firebaseio.com",
+  projectId: "wrabit-webapp",
+  storageBucket: "",
+  messagingSenderId: "493631743586",
+  appId: "1:493631743586:web:98cc1a62b1fbad9f"
+};
+
+export default {
+  prod,
+  stage
+};
--- firebase.js
+++ firebase.js
@@ -1,15 +1,9 @@
 import * as firebase from "firebase/app";
-import "firebase/auth"
+import "firebase/auth";
 
-const config = {
-  apiKey: "AIzaSyD8oE6wim8fdgXW1kg4zFPdwVsqtUdN7Y4",
-  authDomain: "wrabit-webapp.firebaseapp.com",
-  databaseURL: "https://wrabit-webapp.firebaseio.com",
-  projectId: "wrabit-webapp",
-  storageBucket: "",
-  messagingSenderId: "493631743586",
-  appId: "1:493631743586:web:98cc1a62b1fbad9f"
-};
+import firebaseConfig from './firebase.config';
+
+const config = firebaseConfig[process.env.FIREBASE_ENV || 'stage'];
 
 export default !firebase.apps.length
   ? firebase.initializeApp(config)
