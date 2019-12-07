c256deadf08553238e0bb500e5e21147c68be67b
Anthony Morris <anthony@amorrissound.com>
2019-12-07 10:34:52 -0800

    Add better content for upcoming features

--- components/EditorSettings.js
+++ components/EditorSettings.js
@@ -3,7 +3,13 @@ import SectionPanel from '../components/SectionPanel';
 const EditorSettings = () => (
   <div className="w-4/5">
     <SectionPanel>
-      Customizable editor coming soon!
+      <h2 className="text-3xl font-extrabold text-gray-800 mb-5 tracking-wide">
+        Editor
+      </h2>
+
+      <p>
+        Soon you'll be able to customize your editor. Want to hide the tool bar? No problem. Want to show writing prompts? We got you covered. Is there anyting you'd like to see? Let us know in the chat on the bottom right hand corner of the screen!
+      </p>
     </SectionPanel>
   </div>
 );
--- components/NotificationsSettings.js
+++ components/NotificationsSettings.js
@@ -3,7 +3,13 @@ import SectionPanel from '../components/SectionPanel';
 const NotificationsSettings = () => (
   <div className="w-4/5">
     <SectionPanel>
-      Customizable notifications coming soon!
+      <h2 className="text-3xl font-extrabold text-gray-800 mb-5 tracking-wide">
+        Notifications
+      </h2>
+
+      <p>
+        Do you want a friendly reminder to build your daily habit? What about encouragement when you haven't been around for a while? We have plenty in store for you but would love to hear what you want. Let us know in the chat on the bottom right hand corner of the screen!
+      </p>
     </SectionPanel>
   </div>
 );
